1153d23cb3eee8e9f6ef20d6ac4b6608e5974ae6
ikemal12 <iltar2005@gmail.com>
2025-08-27 18:16:42 +0100

    define the classes for the style and content losses

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -29,3 +29,24 @@ assert content_img.size() == style_img.size(), "we need to import style and cont
 unloader = transforms.ToPILImage() # reconvert from tensor to PIL image
 plt.ion()
 
+
+class ContentLoss(nn.Module):
+    def __init__(self, target):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+
+    def forward(self, input):
+        self.loss = F.mse_loss(input, self.target)
+        return input
+    
+class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+    
+def gram_matrix()
