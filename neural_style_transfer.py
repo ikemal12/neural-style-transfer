39807d9bcc115238b402416701c322ee04cf6b96
ikemal12 <iltar2005@gmail.com>
2025-09-21 13:51:13 +0100

    normalization

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -33,12 +33,23 @@ plt.ion()
 class ContentLoss(nn.Module):
     def __init__(self, target):
         super(ContentLoss, self).__init__()
-        self.target = target.detach()
+        self.target = target.detach() # we don't want to backprop through this
 
     def forward(self, input):
         self.loss = F.mse_loss(input, self.target)
         return input
     
+
+# for style loss
+def gram_matrix(input):
+    a, b, c, d = input.size() # a=batch size(=1), b=number of feature maps, (c,d)=dimensions of a f. map (N=c*d)
+
+    features = input.view(a * b, c * d)
+    G = torch.mm(features, features.t()) # compute the gram product
+
+    return G.div(a*b*c*d) # normalize values of gram matrix by dividing by number of element in each feature maps
+    
+
 class StyleLoss(nn.Module):
     def __init__(self, target_feature):
         super(StyleLoss, self).__init__()
@@ -49,4 +60,26 @@ class StyleLoss(nn.Module):
         self.loss = F.mse_loss(G, self.target)
         return input
     
-def gram_matrix()
+    
+# helps to normalize input image so that it can be easily fit into VGG
+class Normalization(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalization, self).__init__()
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        return (img - self.mean) / self.std
+
+    
+# Importing VGG 19 model like in the paper
+cnn = models.vgg19(pretrained=True).features.to(device).eval()
+
+cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
+cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
+
+content_layers_default = ['conv_4']
+style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+
+
